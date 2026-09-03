79a4f2abdd45b8ef3c8eab7e67c73dee55f9920c
jojo8233 <283066271+jojo8233@users.noreply.github.com>
2026-09-03 08:46:54 +0700

    fix(server): exclude colocated migration tests

---
+++ packages/server/src/db/migration-provider.ts
@@ -0,0 +1,16 @@
+import { promises as fs } from 'node:fs'
+import * as path from 'node:path'
+import { FileMigrationProvider, type MigrationProvider } from 'kysely'
+
+const migrationTestFilePattern = /\.test\.[^.]+$/
+
+export function createMigrationProvider(migrationFolder: string): MigrationProvider {
+  return new FileMigrationProvider({
+    fs: {
+      readdir: async directory => (await fs.readdir(directory))
+        .filter(fileName => !migrationTestFilePattern.test(fileName)),
+    },
+    path,
+    migrationFolder,
+  })
+}
